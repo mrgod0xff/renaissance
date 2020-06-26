1c989c7ae3719abe0a5b8378731e7ef0f44b7bfa
Auguinard Kouame <kouameaugui@gmail.com>
2020-06-26 16:46:15 +0000

    first commit pkg static

--- renaissance/settings.py
+++ renaissance/settings.py
@@ -162,7 +162,8 @@ JET_SIDE_MENU_COMPACT = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 STATIC_URL = '/static/'
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
